db10694ffa0aa2b1f7b01408f6acd3a158f5dbeb
zhaohuanwener <718060150@qq.com>
2018-02-28 17:20:42 +0800

    提交项目

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Login,
+      props: {
+        reg: true,
+        title: '注册'
+      }
+    }
+  ]
+})
